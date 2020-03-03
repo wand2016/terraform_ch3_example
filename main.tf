1ee90fd3fd24b23c07a6b5c577589820bea54ab1
Daiki Horiyama <d19921207@gmail.com>
2020-03-04 02:23:38 +0900

    データソースのサンプル

--- main.tf
+++ main.tf
@@ -1,12 +1,23 @@
-locals {
-  example_instance_type = "t3.micro"
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+
+  filter {
+    name = "state"
+    values = ["available"]
+  }
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-0c3fd0f5d33134a76"
-  instance_type = local.example_instance_type
+  ami = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type = "t3.micro"
 }
 
-output "example_instance_id" {
-  value = aws_instance.example.id
+output "example_ami" {
+  value = data.aws_ami.recent_amazon_linux_2.image_id
 }
