ceb71e2de487711871832268c5e94cee4e1fa6c7
Daiki Horiyama <d19921207@gmail.com>
2020-03-04 02:01:13 +0900

    localsとoutputのサンプル

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
-variable "example_instance_type" {
-  default = "t3.micro"
+locals {
+  example_instance_type = "t3.micro"
 }
 
 resource "aws_instance" "example" {
   ami = "ami-0c3fd0f5d33134a76"
-  instance_type = var.example_instance_type
+  instance_type = local.example_instance_type
+}
+
+output "example_instance_id" {
+  value = aws_instance.example.id
 }
