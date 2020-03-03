1ca29bd35f6b35cc0c7ae74a032ed5caa7f8d8aa
Daiki Horiyama <d19921207@gmail.com>
2020-03-04 03:11:09 +0900

    さらに分割してみた

--- http_server/main.tf
+++ http_server/main.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-  region = "ap-northeast-1"
+module "security_group" {
+  source = "../security_group"
 }
 
 variable "instance_type" {}
@@ -7,7 +7,7 @@ variable "instance_type" {}
 resource "aws_instance" "default" {
   ami = data.aws_ami.recent_amazon_linux_2.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.default.id]
+  vpc_security_group_ids = [module.security_group.id]
 
   user_data = file("./user_data.sh")
 }
@@ -26,25 +26,6 @@ data "aws_ami" "recent_amazon_linux_2" {
     values = ["available"]
   }
 }
-
-resource "aws_security_group" "default" {
-  name = "ec2"
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 
 output "public_dns" {
   value = aws_instance.default.public_dns
